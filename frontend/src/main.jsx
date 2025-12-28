77997c2b437cee2e28dcb581c1baefcff6c36dbb
= <=>
2025-12-28 13:32:11 +0530

    update react routing code according to v6

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'
-import { Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
 
 ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode>
-  <Router>
-    <Switch>
-      <Route exact path='/'><App/></Route>
-      <Route exact path='/signup'><Signup/></Route>
-      <Route exact path='./login'><Login/></Route>
-    </Switch>
-  </Router>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App/>}></Route>
+      <Route path='/signup' element={<Signup/>}></Route>
+      <Route path='/login' element={<Login/>}></Route>
+    </Routes>
+  </BrowserRouter>
 </React.StrictMode>)
